60c2ac28e7cb21faf32ed92d06e37074ad75b316
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2025-03-21 21:55:40 -0500

    Some basics

--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,8 @@
 const std = @import("std");
+const RecursiveReal = @import("RecursiveReal.zig");
+const Rational = @import("Rational.zig");
+
+pub const Number = union(enum) {
+    rational: Rational,
+    real: RecursiveReal,
+};
